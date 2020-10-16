dc0ff4e63efb9b6d19a5fa072f1428f3b49dc42c
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-10-17 10:56:18 +1100

    Skeleton code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,47 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const readMeFile = `
+  # ${data.title}
+   ## Table of Contents
+  * [Description](#description)
+  * [Installing Dependencies](#dependencies-how-to)
+  * [Usage](#usage-how-to)
+  * [License](#license-select)
+  * [Contributing](#contributing-how-to)
+  * [Tests](#tests)
 
-`;
+  ## Description
+  ${data.description}
+
+  ## Installing Dependencies
+  Please enter the following: ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributng
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+[GitHub profile link:](https://github.com/${data.username})
+[Email link:](mailto: ${data.email})
+`
+
+
+
+
+
+
+
+ //  return  ${data.title}
+  return readMeFile;
+
 }
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,15 +1,95 @@
+const { prompt } = require("inquirer");
+const goodreadme = require ("./generateMarkdown");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
+
+
+
+
+function promptUser(){
+    // prompt the user for github data.
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your github username"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address"
+    },
+    {
+        type: "input",
+        name: "title",
+        message:  "Please enter the project's title"
+        
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a project description"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Please enter the installation instructions for your project",
+        default: "npm i"
+    },
+    {  type: "input",
+        name: "usage",
+        message: "Please provide details on usage information for your project.",
+    },
+    {
+        type: "list",
+        name: "license",
+        choices:[
+            "Apache License 2.0",
+            "GNU Public License (GPL)",
+            "GNU Library or 'Lesser' General Public License (LGPL)",
+            "MIT License",
+            "Mozilla Public License2.0",
+            "Common Development and Distribution License",
+            "Eclipse Public License version 2.0",
+            "Other"
 
-];
+        ]
+    },
+    { 
+        type: "input",
+        name: "contributing",
+        message: "Please advise of any guidelines in contributing to this project"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide any  testing instructions for this project"
+    }]
+    return prompt(questions);
+}
+    
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(filename, data);
 }
 
 // function to initialize program
-function init() {
-
+async function init() {
+    try{
+        const answers = await promptUser();
+        const readmeFile = generateMarkdown(answers);
+   
+        console.log(answers);
+        
+        writeToFile( readmeFile, answers);
+        
+    }
+    catch (err){
+        console.log(err);
+    }
+    console.log("The README was created successfully"); 
 }
 
 // function call to initialize program
